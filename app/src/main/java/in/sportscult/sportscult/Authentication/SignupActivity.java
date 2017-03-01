98bd45841341e1e31af99c6aebc4203f3a552551
Vishal Gautam <vishalgautam.dtu@gmail.com>
2017-03-01 12:06:11 +0530

    Bugs Fixed

--- app/src/main/java/in/sportscult/sportscult/Authentication/SignupActivity.java
+++ app/src/main/java/in/sportscult/sportscult/Authentication/SignupActivity.java
@@ -30,6 +30,7 @@ import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
